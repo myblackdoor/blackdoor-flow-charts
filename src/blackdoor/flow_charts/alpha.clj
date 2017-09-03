9d5e4290b2c7eed4ce8c173425d5c5f5737d4c86
Dylan Butman <dbutman@gmail.com>
2017-09-02 19:55:45 -0500

    flows

--- src/blackdoor/flow_charts/alpha.clj
+++ src/blackdoor/flow_charts/alpha.clj
@@ -3,7 +3,8 @@
             [me.raynes.fs :as fs]
             [plumbing.core :refer :all]
             [rhizome.viz :as viz]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [clojure.set :as set]))
 
 (def flows
   {:public
@@ -13,13 +14,13 @@
                       :typeform/subscribe]
     :blackdoor/member [:slack/signin
                        :loomio/signin]
-    :loomio/signin [:loomio/subgroups]
+    :loomio/signin [:loomio.subgroups/blackdoor]
     :slack/signin [:slack/commands]
     :facebook/myblackdoor [:com/myblackdoor]
     :instagram/myblackdoor [:com/myblackdoor]}
    :subscribe
    {:typeform/subscribe [:emails/subscribed
-                         :mailchimp/blackdoor]
+                         :mailcimp/blackdoor]
     :emails/subscribed [:patreon/blackdoor]}
    :invite
    {:typeform/invite [:emails/invite]
@@ -31,53 +32,67 @@
     :loomio.subgroups/applications [:loomio.subgroups.applications/approve
                                     :loomio.subgroups.applications/reject]
     :loomio.subgroups.applications/approve [:emails/accepted]
-    :emails/accepted [:slackpass/signup]
-    :loomio.subgroups.applications/reject [:emails/rejected]}
+    :emails/accepted [:typeform/signup]
+    :loomio.subgroups.applications/reject [:emails/rejected]
+    :emails/rejected []}
    :signup
-   {:slackpass/signup [:emails/signed-up
+   {:typeform/signup [:docs/terms-of-service]
+    :docs/terms-of-service [:slackpass/signup]
+    :slackpass/signup [:emails/signed-up
                        :docs/orientation
                        :loomio/new-user
                        :slack/new-user
                        :trello/new-user
-                       :google-drive/new-user]
+                       :google-drive/share]
     :emails/signed-up [:docs/orientation
                        :calendly.schedule/onboarding
                        :patreon/blackdoor]
+    :calendly.schedule/onboarding []
+    :loomio/new-user []
+    :slack/new-user []
+    :trello/new-user []
+    :google-drive/share []}
+   :docs
+   {:google-drive/blackdoor [:docs/orientation
+                             :docs/guidelines
+                             :docs/user-guide
+                             :docs/faq]
     :docs/orientation [:calendly.schedule/onboarding
-                       :patreon/blackdoor
-                       :loomio.subgroups/blackdoor
-                       :slack.channels/blackdoor
-                       :trello/blackdoor
-                       :google-drive/blackdoor]}
-   :report
-   {:typeform/report [:loomio.subgroups/moderation]}
+                       :docs/guidelines
+                       :docs/user-guide
+                       :docs/faq
+                       :patreon/blackdoor]
+    :docs/guidelines []
+    :docs/faq []
+    :docs/user-guide []}
+   :moderation
+   {:typeform/report [:loomio.subgroups/moderation]
+    :loomio.subgroups/moderation [:loomio.subgroups.moderation/allow
+                                  :loomio.subgroups.moderation/reprimand
+                                  :loomio.subgroups.moderation/ban]
+    :loomio.subgroups.moderation/allow []
+    :loomio.subgroups.moderation/reprimand [:emails/reprimand]
+    :emails/reprimand [:calendly.schedule/talk]
+    :loomio.subgroups.moderation/ban [:emails/ban]
+    :emails/ban [:calendly.schedule/talk
+                 :slack/remove-user
+                 :loomio/remove-user
+                 :trello/remove-user
+                 :google-drive/unshare]
+    :slack/remove-user []
+    :loomio/remove-user []
+    :trello/remove-user []
+    :google-drive/unshare []}
    :slack
-   {:slack/channels [:slack.channels/blackdoor
-                     :slack.channels/story-time
-                     :slack.channels/health-safety
-                     :slack.channels/tech]
-    :slack/commands [:slack.commands/appear
+   {:slack/commands [:slack.commands/appear
                      :slack.commands/invite
                      :slack.commands/report
                      :slack.commands/talk]
     :slack.commands/appear [:appearin/new-video-conference]
+    :appearin/new-video-conference []
     :slack.commands/invite [:typeform/invite]
     :slack.commands/report [:typeform/report]
-    :slack.commands/talk [:calendly.schedule/talk]}
-   :calendly
-   {:calendly/schedule [:calendly.schedule/onboarding
-                        :calendly.schedule/talk
-                        :calendly.schedule/pleasetrythisathome
-                        :calendly.schedule/sspectacularr]}
-   :loomio
-   {:loomio/subgroups [:loomio.subgroups/blackdoor
-                       :loomio.subgroups/applications
-                       :loomio.subgroups/moderation
-                       :loomio.subgroups/tech]}
-   :integrations
-   {:loomio/subgroups [:slack.channels/blackdoor]
-    :github/changes [:slack.channels/blackdoor]
-    :google-drive/blackdoor [:slack.channels/blackdoor]}})
+    :slack.commands/talk [:calendly.schedule/talk]}})
 
 (def alpha
   (let [flattened (apply merge-with (comp vec concat)
@@ -88,10 +103,6 @@
          (distinct)
          (<- (zipmap (repeat [])))
          (merge-with (comp vec concat) flattened))))
-
-(def dir "graphs")
-(fs/delete-dir dir)
-(fs/mkdirs dir)
 
 (defn node->key
   [s]
@@ -123,32 +134,76 @@
   (or (cluster->parent (key->parent k))
       (key->parent k)))
 
-(defn viz
-  []
-  (viz/save-graph
-   (keys alpha) alpha
-   :filename (str dir "/alpha.png")
-   :node->descriptor (partial hash-map :label))
-  (viz/save-graph
-   (keys alpha) alpha
-   :filename (str dir "/alpha-clusters.png")
-   :node->descriptor (partial hash-map :label)
-   :cluster->descriptor (partial hash-map :label)
-   :node->cluster (comp top-cluster node->key))
-  (viz/save-graph
-   (keys alpha) alpha
-   :filename (str dir "/alpha-clusters-parents.png")
-   :node->descriptor (partial hash-map :label)
-   :cluster->descriptor (partial hash-map :label)
-   :node->cluster (comp key->parent node->key)
-   :cluster->parent cluster->parent))
+(defn nodes-in-flow
+  [k]
+  (let [flow (get flows k)
+        flow-nodes (keys flow)
+        after-flow (->> (vals flow)
+                        (mapcat identity))
+        before-flow (->> alpha
+                         (filter (comp seq
+                                       (partial set/intersection (set flow-nodes))
+                                       set
+                                       second))
+                         (map first))]
+    (distinct (concat flow-nodes after-flow before-flow))))
 
-(viz)
+(defn flow-for-node
+  [node]
+  (first
+   (for [[k flow] flows
+         :when (get (set (keys flow)) node)]
+     k)))
+
+(->> flows
+     (vals)
+     (mapcat identity)
+     (map first)
+     distinct
+     (map )
+     (filter (comp seq next second)))
+
+(def dir "graphs")
+(fs/delete-dir dir)
+(fs/mkdirs dir)
+
+(viz/save-graph
+ (keys alpha) alpha
+ :filename (str dir "/alpha.png")
+ :node->descriptor (partial hash-map :label))
+
+(viz/save-graph
+ (keys alpha) alpha
+ :filename (str dir "/alpha-flows.png")
+ :node->descriptor (partial hash-map :label)
+ :cluster->descriptor (partial hash-map :label)
+ :node->cluster (comp flow-for-node node->key))
+
+(viz/save-graph
+ (keys alpha) alpha
+ :filename (str dir "/alpha-services.png")
+ :node->descriptor (partial hash-map :label)
+ :cluster->descriptor (partial hash-map :label)
+ :node->cluster (comp top-cluster node->key))
+
+(viz/save-graph
+ (keys alpha) alpha
+ :filename (str dir "/alpha-services-parents.png")
+ :node->descriptor (partial hash-map :label)
+ :cluster->descriptor (partial hash-map :label)
+ :node->cluster (comp key->parent node->key)
+ :cluster->parent cluster->parent)
+
+(fs/mkdirs (str dir "/flows"))
+
+(doseq [flow (keys flows)]
+  (viz/save-graph
+   (nodes-in-flow flow) alpha
+   :filename (str dir "/flows/" (name flow) ".png")
+   :node->descriptor (partial hash-map :label)))
 
 (comment
-  (viz/view-graph (keys alpha) alpha
+  (viz/view-graph (keys (:public flows)) alpha
                   :node->descriptor (fn [n] {:label n}))
-
-
 
   )
